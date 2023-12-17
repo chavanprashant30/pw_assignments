b49436d062f68194c74a6a32f8f4530df24f7468
Prashant Chavan <chavanprashant3097@gmail.com>
2023-12-17 21:19:01 +0530

    Add sixth question

---
+++ 04_nov_introToJSOps/Q5_simpleIntrest.js
@@ -0,0 +1,11 @@
+// Q5. Write a JavaScript program that calculates the simple interest using the formula Simple interest =
+// (principal * rate * time) / 100.
+// Ans:
+    // S.I. = (P × R × T)/100
+
+    const p =5000;
+    const r = 5;
+    const t = 10;
+
+    const simpleIntrest =(p * r * t)/100;
+    console.log(`SimpleIntrest is : ${simpleIntrest}`);
---
+++ 04_nov_introToJSOps/Q6_BMI_Calculator.js
@@ -0,0 +1,10 @@
+// Q6. Write a Javascript program to calculate the Body Mass Index (BMI) using the formula BMI = weight (kg)/
+// height * height.
+// Ans:
+    // const BMI = weight/ height * height
+
+    const weight = 68;
+    const height = 170.18/100;
+
+    const bmi = weight/ (height * height);
+    console.log(`Your BMI is : ${bmi}`);
