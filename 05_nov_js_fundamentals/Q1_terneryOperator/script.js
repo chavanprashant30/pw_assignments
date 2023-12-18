77a1222f232fde3e146dd590e97aad4fa265378f
Prashant Chavan <chavanprashant3097@gmail.com>
2023-12-18 23:11:00 +0530

    Add first question for 5th nov assignment

---
+++ 05_nov_js_fundamentals/Q1_terneryOperator/script.js
@@ -0,0 +1,23 @@
+// Q1).Explain the usage of ternary operator with syntax and write a program to check whether the number is even or odd using the ternary operator.
+
+// Ans:
+//The conditional (ternary) operator is the only JavaScript operator that takes three operands: a condition followed by a question mark (?), then an expression to execute if the condition is truthy followed by a colon (:), and finally the expression to execute if the condition is falsy. This operator is frequently used as an alternative to an if...else statement.
+
+// Example: 
+
+    // function age(myAge){
+    //     myAge>18 ? console.log("You can drive") : console.log("You can't drive");
+    // }
+
+    // age(20);
+    // age(17);
+
+
+// Program for number is even or odd check.
+
+function evenOdd(no){
+    no % 2 == 0 ? console.log("Number is even") : console.log("Number is odd");
+}
+
+evenOdd(10);
+evenOdd(9)
