436bc06932132fcb2b4b6733329c8cdde9da6eba
Prashant Chavan <chavanprashant3097@gmail.com>
2023-12-18 23:45:29 +0530

    Added third question and ist output

---
+++ 05_nov_js_fundamentals/Q3_nestedTernory/style.js
@@ -0,0 +1,14 @@
+// Q3).Use a nested ternary operator to check that a number is positive, negative or zero. You have to print
+// “positive” if the number is positive and similarly for negative and zero also.
+
+// Ans:
+
+let number = 11;
+const noType =
+number > 0  
+    ? "Positive"
+    : number < 0
+    ? "Negative"
+    : "Zero"
+
+    console.log(noType);
