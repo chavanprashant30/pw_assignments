8dc3ade7c53b239572db87016fb02e983d753552
Prashant Chavan <chavanprashant3097@gmail.com>
2023-12-18 23:53:51 +0530

    Add question fourth and its output

---
+++ 05_nov_js_fundamentals/Q4_eligiblityForVote/script.js
@@ -0,0 +1,10 @@
+// Q4).Use a ternary operator to check that a person is eligible to vote or not by checking his age. If the age of the
+// person is less than 18 then “You cannot vote” should be logged else “You can vote” should be logged.
+
+// Ans:
+
+const age = 2;
+
+const result = age > 18 ? "can Vote" : "Can't Vote";
+
+console.log("You", result);
